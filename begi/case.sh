d1aa4bb4791c8693c8dc297d0b8cd0beb08403e9
utokrikko <kricksc@gmail.com>
2023-02-15 12:21:09 -0600

    case statment

---
+++ begi/case.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+finished=0
+
+while [ $finished -ne 1 ]; do
+    echo "what is your favorite linux distribution"
+    echo "1 -Arch"
+    echo "2 CentOS "
+    echo "3 - Debian"
+    echo "4 - Mint"
+    echo "5 - Ubuntu"
+    echo "6 - something esle.."
+
+    read distro;
+
+    case $distro in 
+        1) echo "Arch is a rolling release" ;;
+        2) echo "centOS is popular on servers" ;;
+        3) echo "ebian is a community distribution" ;;
+        4) echo "Mint is popular on desktops and laptops." ;;
+        5) echo "Ubuntu is popular on both servers and computers" ;;
+        6) "There are many distributions out there" ;;
+        7) finished=1 ;;
+        *) echo "you didn't enter an apprioprate choice."
+    esac
+done 
